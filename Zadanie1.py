6da0d6a577c7a81f66a6e9d3d0b83ac66663731d
Mark Kuznetsov <markkuznetsov74@gmail.com>
2023-09-14 13:24:43 +0300

    Added comments

--- Zadanie1.py
+++ Zadanie1.py
@@ -1,15 +1,15 @@
-numbers_example = 1+1
-string_example = "a" * 4
+numbers_example = 1+1 #оператор с числом
+string_example = "a" * 4 #оператор с строкой
 
 list_example = list()
-list_example.append(1)
+list_example.append(1) #метод листа
 
 array1 = [1,2,4]
 array2 = [2,3,5]
-result_array = array2 + array1
+result_array = array2 + array1 #оператор и массивами
 
 tuple1 = (1,2)
 tuple2 = (2,3) 
-result_tuple = tuple1+ tuple2
+result_tuple = tuple1+ tuple2 #оператор и кортежи
 
 print(numbers_example, string_example, list_example, result_array, result_tuple)
