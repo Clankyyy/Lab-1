693546c46626c12850d3538e87825e1418940d2a
Mark Kuznetsov <markkuznetsov74@gmail.com>
2023-09-14 13:19:56 +0300

    Comment

---
+++ Zadanie1.py
@@ -0,0 +1,15 @@
+numbers_example = 1+1
+string_example = "a" * 4
+
+list_example = list()
+list_example.append(1)
+
+array1 = [1,2,4]
+array2 = [2,3,5]
+result_array = array2 + array1
+
+tuple1 = (1,2)
+tuple2 = (2,3) 
+result_tuple = tuple1+ tuple2
+
+print(numbers_example, string_example, list_example, result_array, result_tuple)
